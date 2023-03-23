bfe1bd71ee8cec9cdbaf7133d150eba3ed60011d
k123s456h <k123s456h@gmail.com>
2023-03-24 00:36:40 +0900

    fix: simplify scripts dir

---
+++ scripts/run.server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+SRC_DIR="$SCRIPT_DIR/.."
+
+cd $SRC_DIR
+
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
